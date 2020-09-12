612119d225d1e3c4d3181c899991b235fe3fd15b
Adsana <adsanasayasith@gmail.com>
2020-09-12 11:37:50 -0400

    const requires

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require('fs');
+const path = require('path');
+async function main(){
+    console.log(`starting`);
+    const userResponse = await inquirer
+    
